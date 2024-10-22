52b9f3f4ae777444136287574f11a6d40504613e
Petra Puljić <ppuljicc@gmail.com>
2024-10-22 23:03:19 +0200

    Samostalni zadatak za vježbu 1
    Izmijenite vaš Express poslužitelj tako da:
    
    1. Nadogradite postojeću GET rutu / koja sad mora vratiti HTML stranicu s <h1> naslovom "Hello,
    Express!".
    
    2. Dodate još jednu GET rutu /about koja će vratiti HTML stranicu s <h1> naslovom "Ovo je stranica o
    nama!".
    Obje HTML stranice pohranite u direktorij /public .
    Kako biste vratili podatke u obliku HTML stranice, koristite res.sendFile() metodu.
    Sintaksa: res.sendFile(__dirname + "putanja_do_datoteke");
    
    3. Dodajte i posljednju GET rutu /users koja će vratiti korisnike u JSON formatu. Korisnike pohranite u
    polju kao objekte s atributima id , ime i prezime . Dodajte barem 3 korisnika. Kako biste vratili
    korisnike u JSON formatu, koristite res.json() metodu.
    
    Testirajte u web pregledniku i s programom curl sve tri rute

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get("/", (reg, res) => {
-    res.send("Hello World!");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "about.html"));
+})
+
+app.get("/users", (req, res) => {
+    const users = [
+        { id: 1, ime: "Ana", prezime: "Anić" },
+        { id: 2, ime: "Marko", prezime: "Markić" },
+        { id: 3, ime: "Luka", prezime: "Lukić" }
+    ];
+    res.json(users);
 });
 
 app.listen(PORT, (error) => {
