acfb90ecf906a919dbf69a2e50d15bc45b586e18
Petra Puljić <ppuljicc@gmail.com>
2024-11-05 15:33:51 +0100

    Samostalni zadatak za Vježbu 2
    Definirajte novi Express projekt u kojem ćete implementirati jednostavni poslužitelj za agenciju za nekretnine.
    
    Osmislite dizajn poslužitelja, a podatke spremajte u polje objekata, odnosno in-memory. Podaci o nekretninama trebaju sadržavati sljedeće informacije:
    - ID nekretnine
    - Naziv nekretnine
    - Opis nekretnine
    - Cijena nekretnine
    - Lokacija nekretnine
    - Broj soba
    - Površina nekretnine
    - Cijena nekretnine
    
    Implementirajte sljedeće rute:
    - dohvati sve nekretnine
    - dohvati nekretninu po ID-u
    - dodaj novu nekretninu
    - ažuriraj nekretninu potpuno
    - ažuriraj nekretninu djelomično
    - obriši nekretninu
    - pošalji novu ponudu
    
    Ponude spremajte na jednak način u polje objekata, a svaka ponuda mora sadržavati:
    - ID ponude
    - ID nekretnine
    - Ime kupca
    - Prezime kupca
    - Ponuđena cijena
    - Broj telefona kupca
    
    Dodajte slične provjere kao u pizzeriji, primjerice:
    - provjerite jesu li ID-evi brojevi, ako ne vratite odgovarajući statusni kod i poruku
    - provjerite jesu li svi podaci poslani u tijelu zahtjeva, ako nisu vratite odgovarajući statusni kod i poruku
    - provjerite jesu li svi podaci ispravni, npr. cijena nekretnine ne može biti negativna, broj soba ne može biti negativan, itd.
    - prilikom izrade ponude, provjerite postoji li uopće nekretnina s navedenim ID-em
    
    Rute za nekretnine i ponude grupirajte u zasebne Router objekte i organizirajte ih u zasebnim datotekama unutar routes direktorija. Koristite statusne kodove u odgovorima.
    Za testiranje koristite Postman ili Thunder Client.

---
+++ Samostalni zadatak za vježbu 2/data/nekretnine.js
@@ -0,0 +1,95 @@
+const nekretnine = [
+    {
+        id: 1,
+        naziv: "Stan u Zagrebu",
+        opis: "Predivan stan u centru Zagreba.",
+        cijena: 150000,
+        lokacija: "Zagreb",
+        brojSoba: 2,
+        povrsina: 60
+    },
+    {
+        id: 2,
+        naziv: "Garsonijera u Puli",
+        opis: "Mala garsonijera u samom centru Pule.",
+        cijena: 135000,
+        lokacija: "Pula",
+        brojSoba: 1,
+        povrsina: 30
+    },
+    {
+        id: 3,
+        naziv: "Kuća u Splitu",
+        opis: "Prostrana kuća sa vrtom u blizini plaže.",
+        cijena: 350000,
+        lokacija: "Split",
+        brojSoba: 4,
+        povrsina: 120
+    },
+    {
+        id: 4,
+        naziv: "Apartman u Dubrovniku",
+        opis: "Luksuzni apartman s pogledom na more.",
+        cijena: 500000,
+        lokacija: "Dubrovnik",
+        brojSoba: 2,
+        povrsina: 80
+    },
+    {
+        id: 5,
+        naziv: "Stan u Rijeci",
+        opis: "Moderni stan u blizini svih sadržaja.",
+        cijena: 190000,
+        lokacija: "Rijeka",
+        brojSoba: 3,
+        povrsina: 70
+    },
+    {
+        id: 6,
+        naziv: "Kuća u Opatiji",
+        opis: "Idilična kuća sa predivnim vrtom.",
+        cijena: 400000,
+        lokacija: "Opatija",
+        brojSoba: 5,
+        povrsina: 150
+    },
+    {
+        id: 7,
+        naziv: "Stan u Osijeku",
+        opis: "Prostrani stan u mirnom kvartu.",
+        cijena: 120000,
+        lokacija: "Osijek",
+        brojSoba: 3,
+        povrsina: 75
+    },
+    {
+        id: 8,
+        naziv: "Vikendica u Istri",
+        opis: "Savršena vikendica za odmor u prirodi.",
+        cijena: 250000,
+        lokacija: "Istra",
+        brojSoba: 2,
+        povrsina: 90
+    },
+    {
+        id: 9,
+        naziv: "Stan u Zadru",
+        opis: "Stan u blizini plaže i starih gradskih zidina.",
+        cijena: 210000,
+        lokacija: "Zadar",
+        brojSoba: 2,
+        povrsina: 55
+    },
+    {
+        id: 10,
+        naziv: "Kuća u Slavoniji",
+        opis: "Tradicionalna kuća sa velikim dvorištem.",
+        cijena: 180000,
+        lokacija: "Slavonija",
+        brojSoba: 4,
+        povrsina: 130
+    }
+];
+
+
+export default nekretnine;
