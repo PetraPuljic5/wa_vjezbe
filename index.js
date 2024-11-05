bdbc3865c7718cc6112e70ae7b2b04e2101e13e0
Petra Puljić <ppuljicc@gmail.com>
2024-11-05 13:20:21 +0100

    Vježbe 5.11.
    PUT, PATCH, Router

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
 const app = express();
+import pizzeRouter from './routes/pizze.js';
 
 app.use(express.json());
+app.use('/pizze', pizzeRouter); 
 
 const PORT = 3001;
 
@@ -12,7 +14,7 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "about.html"));
-})
+});
 
 app.get("/users", (req, res) => {
     const users = [
@@ -21,133 +23,6 @@ app.get("/users", (req, res) => {
         { id: 3, ime: "Luka", prezime: "Lukić" }
     ];
     res.json(users);
-});
-
-const pizze = [
-    { id: 1, naziv: 'Margherita', cijena: 6.5 },
-    { id: 2, naziv: 'Capricciosa', cijena: 8.0 },
-    { id: 3, naziv: 'Quattro formaggi', cijena: 10.0 },
-    { id: 4, naziv: 'Šunka sir', cijena: 7.0 },
-    { id: 5, naziv: 'Vegetariana', cijena: 9.0 }
-];
-
-app.get('/pizze', (req, res) => {
-    res.json(pizze);
-});
-
-app.get('/pizze/:id', (req, res) => {
-    const id_pizza = req.params.id;
-    if (isNaN(id_pizza)){
-        res.json({ message: 'Proslijedili ste parametar id koji nije broj!' });
-        return;
-    }
-
-    const pizza = pizze.find(pizza => pizza.id == id_pizza);
-    if (pizza) {
-        res.json(pizza);
-    } else {
-        res.json({ message: 'Pizza s traženim ID-em ne postoji.' });
-    }
-});
-
-let narudzbe = [];
-
-app.post('/naruci', (req, res) => {
-    const narudzba = req.body;
-
-    if (!Array.isArray(narudzba.narudzba) || narudzba.narudzba.length === 0 || !narudzba.prezime || !narudzba.adresa || !narudzba.broj_telefona) {
-        res.status(400).json({ message: 'Niste poslali ispravne podatke za narudžbu!' });
-        return;
-    }
-
-    let nepostojecePizze = [];
-    let ispravneNarudzbe = [];
-    let ukupnaCijena = 0;
-
-    for (const item of narudzba.narudzba) {
-        if (!item.pizza || !item.velicina || !item.kolicina) {
-            res.status(400).json({ message: 'Jedan ili više objekata nema sve potrebne podatke!' });
-            return;
-        }
-
-        const postojiPizza = pizze.find(pizza => pizza.naziv === item.pizza);
-        if (postojiPizza) {
-            ispravneNarudzbe.push(item);
-            ukupnaCijena += postojiPizza.cijena * item.kolicina;
-        } else {
-            nepostojecePizze.push(item.pizza);
-        }
-    }
-
-    if (nepostojecePizze.length > 0) {
-        res.status(400).json({
-            message: `Jedna ili više pizza koje ste naručili ne postoji: ${nepostojecePizze.join(", ")}`
-        });
-        return;
-    }
-
-    narudzbe.push({
-        narudzba: ispravneNarudzbe,
-        prezime: narudzba.prezime,
-        adresa: narudzba.adresa,
-        broj_telefona: narudzba.broj_telefona
-    });
-
-    const naziviPizza = ispravneNarudzbe.map(item => `${item.pizza} (${item.velicina})`).join(" i ");
-    
-    res.json({
-        message: `Vaša narudžba za ${naziviPizza} je uspješno zaprimljena!`,
-        prezime: narudzba.prezime,
-        adresa: narudzba.adresa,
-        ukupna_cijena: ukupnaCijena
-    });
-});
-
-app.put('/pizze/:id', (req, res) => {
-    let id_pizza_req = req.params.id;
-    let tijelo_zahtjeva = req.body;
-
-    if (isNaN(id_pizza_req)) {
-        res.json({ message: 'Proslijedili ste parametar id koji nije broj!' });
-        return;
-    }
-
-    let index = pizze.findIndex(pizza => pizza.id = id_pizza_req);
-    pizze[index] = tijelo_zahtjeva;
-
-    return res.json({message:'Ažurirano!'})
-});
-
-app.patch('/pizze/:id', (req, res) => {
-    let id_pizza_req = req.params.id;
-    let tijelo_zahtjeva = req.body;
-
-    if (isNaN(id_pizza_req)) {
-        res.json({ message: 'Proslijedili ste parametar id koji nije broj!' });
-        return;
-    }
-
-    let index = pizze.findIndex(pizza => pizza.id = id_pizza_req);
-
-    let kljucevi_objekta = Object.keys(tijelo_zahtjeva);
-
-    console.log(kljucevi_objekta);
-
-    for (let kljuc of kljucevi_objekta){
-        if (pizze[index][kljuc] != tijelo_zahtjeva[kljuc]) {
-        pizze[index][kljuc] = tijelo_zahtjeva[kljuc]
-    } else {
-        pizze[index][kljuc] = pizze[index][kljuc];
-    };
-
-
-    pizze[index][kljuc]=tijelo_zahtjeva[kljuc];
-}
-
-pizze[index] = tijelo_zahtjeva;
-console.log(pizze);
-
-return res.json({message:'Azurirano!',azurirani_podatak: tijelo_zahtjeva})
 });
 
 app.listen(PORT, (error) => {
